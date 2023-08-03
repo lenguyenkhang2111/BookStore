5cc4e5e3f25252d77144192f043eb1f819d357ee
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2023-08-03 23:37:43 +0700

    Update model

---
+++ Migrations/20230803163714_UpdateModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DatePublished",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DiscountPercentage",
+                table: "Books",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DatePublished",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountPercentage",
+                table: "Books");
+        }
+    }
+}
