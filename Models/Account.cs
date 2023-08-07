2b3610c373496865477c0d7d5ca53b9761ec5b1c
ThuVi <buihhh321@gmail.com>
2023-08-07 14:58:28 +0700

    Login, Register customer Complete

---
+++ Models/Account.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+
+        public class Register
+        {
+            [Required]
+            public string FullName { get; set; }
+
+            [Required]
+            public string HomeAddress { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "Not Match")]
+            public string ConfirmPassword { get; set; }
+        }
+            public class Login
+            {
+                [Required]
+                [EmailAddress]
+                public string Email { get; set; }
+
+                [Required]
+                [DataType(DataType.Password)]
+                public string Password { get; set; }
+
+                [Display(Name = "Remember me")]
+                public bool RememberMe { get; set; }
+            }
+
+            public class ProfileViewModel
+            {
+                public string FullName { get; set; }
+                public string HomeAddress { get; set; }
+                    
+                [EmailAddress]
+                public string Email { get; set; }
+
+            }
+
+            public class EditProfileViewModel
+            {
+                [Required]
+                [EmailAddress]
+                public string Email { get; set; }
+
+                [Required]
+                public string FullName { get; set; }
+
+                [Required]
+                public string HomeAddress { get; set; }
+            }
+
+
+
+
+    public class ChangePasswordViewModel
+                {
+                [Required]
+                [DataType(DataType.Password)]
+                [Display(Name = "Current Password")]
+                public string CurrentPassword { get; set; }
+
+                [Required]
+                [DataType(DataType.Password)]
+                [Display(Name = "New Password")]
+                public string NewPassword { get; set; }
+
+                [DataType(DataType.Password)]
+                [Display(Name = "Confirm New Password")]
+                [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+                public string ConfirmNewPassword { get; set; }
+                }
+
+                
+
+
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BookStore.Data;
+using BookStore.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
@@ -11,9 +12,13 @@ builder.Services.AddDbContext<StoreDbContext>(opts =>
 {
     opts.UseSqlite(builder.Configuration.GetConnectionString("BookStoreSQLiteConnection"));
 });
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<StoreDbContext>();
+builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<StoreDbContext>();
 
-
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+});
 
 var app = builder.Build();
 
@@ -29,7 +34,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
