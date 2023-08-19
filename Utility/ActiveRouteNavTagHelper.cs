4327fbae4def8232b83aac6bf5dd6b29cd5a55ba
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2023-08-20 02:11:41 +0700

    Complete to checkout

---
+++ Utility/ActiveRouteNavTagHelper.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BookStore.Utility;
+[HtmlTargetElement("li", Attributes = "asp-controller,asp-action, active-route")]
+public class ActiveRouteTagHelper : TagHelper
+{
+
+    [ViewContext]
+    [HtmlAttributeNotBound]
+    public ViewContext? ViewContext { get; set; }
+
+    [HtmlAttributeName("asp-controller")]
+    public string? Controller { get; set; }
+
+    [HtmlAttributeName("asp-action")]
+    public string? Action { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if (ViewContext != null)
+        {
+            var currentController = ViewContext.RouteData.Values["controller"]?.ToString();
+            var currentAction = ViewContext.RouteData.Values["action"]?.ToString();
+
+            if (Controller == currentController && Action == currentAction)
+            {
+                output.Attributes.Add("class", "active");
+            }
+
+        }
+    }
+}
+
